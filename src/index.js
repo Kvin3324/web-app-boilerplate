67fc3eee020e170dff35d63f71f83ce919382d0b
kevin joya <kevin.joya@outlook.com>
2019-02-03 21:00:15 +0100

    Added timer

--- src/index.js
+++ src/index.js
@@ -3,3 +3,29 @@
  */
 import "./assets/scss/main.scss";
 
+
+/**
+ * TIMER
+ */
+const begin = setInterval(function () { myTimer() }, 1000);
+let secondlimit = 5;
+
+function myTimer() {
+    if (secondlimit == 0) {
+        myStopFunction();
+    }
+
+    document.getElementById("timer").innerHTML = '00:' + zeroPad(secondlimit, 2);
+    secondlimit = secondlimit - 1;
+
+}
+
+function myStopFunction() {
+    clearInterval(begin);
+    alert('You are to slow... Try again !')
+}
+
+function zeroPad(num, places) {
+    const zero = places - num.toString().length + 1;
+    return Array(+(zero > 0 && zero)).join("0") + num;
+}
